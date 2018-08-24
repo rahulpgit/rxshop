6fc2c2a16fc9857abee909c68ffaf6ceab2e3fd3
Unknown <rahul.p@psychz.net>
2018-08-24 20:29:15 +0530

    RxShop Application

---
+++ src/app/admin/services/admin-auth-guard.service.ts
@@ -0,0 +1,35 @@
+import { UserService } from 'shared/services/user.service';
+import { AuthService } from 'shared/services/auth.service';
+import { Observable } from 'rxjs/Observable';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AdminAuthGuardService implements CanActivate {
+
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.appUser$
+      .map(user => {
+        if ( null !== user &&
+          ( 'rxshop.makex@gmail.com' === user.email ||
+          'rxshop.makey@gmail.com' === user.email ) ) {
+          user.isAdmin = true;
+        }
+
+        if (user.isAdmin === true) {
+          return true;
+        }
+
+        this.router.navigate(['\login'], {
+          queryParams: {
+            returnUrl: state.url
+          }
+        });
+
+        return false;
+      });
+  }
+}
